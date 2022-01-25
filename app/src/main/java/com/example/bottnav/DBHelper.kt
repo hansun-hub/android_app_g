abf5537bd57ffca27d61b75b5879c8c051fbc02e
nusuy <yusun15@naver.com>
2022-01-26 00:51:32 +0900

    db second commit

--- app/src/main/java/com/example/bottnav/DBHelper.kt
+++ app/src/main/java/com/example/bottnav/DBHelper.kt
@@ -9,8 +9,10 @@ class DBHelper(context: Context): SQLiteOpenHelper(context, "database1", null, 1
     var user_email = context!!.getSharedPreferences("current", Context.MODE_PRIVATE).getString("email", "")
 
     override fun onCreate(db: SQLiteDatabase?) {
+        // 사용자 정보 table 생성
+        db!!.execSQL("CREATE TABLE IF NOT EXISTS USERS (email CHAR(15), nickname CHAR(15), password TEXT);")
         // 미션 달성 정보 table 생성
-        db!!.execSQL("CREATE TABLE IF NOT EXISTS \"ACHIEVE_$user_email\" (date DATE, hash INTEGER, is_achieved CHAR);")
+        db!!.execSQL("CREATE TABLE IF NOT EXISTS \"ACHIEVE_$user_email\" (date DATE, type CHAR, i INTEGER, is_achieved CHAR);")
         // 소감 table 생성
         db!!.execSQL("CREATE TABLE IF NOT EXISTS \"DIARY_$user_email\" (data DATE, title TEXT, contents TEXT, score INTEGER, selected TEXT)")
     }
